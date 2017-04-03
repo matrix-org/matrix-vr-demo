4ae8036c799e5c4ce88a27f11e4925a16efc1036
Robert Swain <rob@matrix.org>
2017-04-03 12:19:52 +0200

    Login: Add support for specifying peer id in query parameter

--- src/js/components/structures/utils.js
+++ src/js/components/structures/utils.js
@@ -32,3 +32,19 @@ export function createVideoElement(isLocal, id) {
     }
     return el;
 }
+
+export function getParameterByName(name, url) {
+    if (!url) {
+        url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, '\\$&');
+    const regex = new RegExp('[?&]' + name + '(=([^&]*)|&|$)');
+    const results = regex.exec(url);
+    if (!results) {
+        return null;
+    }
+    if (!results[2]) {
+        return '';
+    }
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
