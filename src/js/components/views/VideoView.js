dfe22fba1333395b78b27fd410457a6447061bf7
Robert Swain <rob@matrix.org>
2017-04-19 11:48:00 +0200

    CallView: VideoView: Display informational text when there is no video

--- src/js/components/views/VideoView.js
+++ src/js/components/views/VideoView.js
@@ -45,13 +45,19 @@ export default class VideoView extends React.Component {
     render() {
         let text;
         if (this.props.text.length > 0) {
+            const textValue = this.props.hasVideo ?
+                this.props.text :
+                `No video for:\n${this.props.text}`;
+            const textYPos = this.props.hasVideo ?
+                -(this.props.height / 2) - 0.1 :
+                0.0;
             text = <a-text
-                    value={this.props.text}
+                    value={textValue}
                     width='3'
                     scale='0 0 0'
                     color='#ffffff'
                     align='center'
-                    position={[0, -(this.props.height / 2) - 0.1, 0].join(' ')}>
+                    position={[0, textYPos, 0].join(' ')}>
                 <a-animation
                     attribute='opacity'
                     dur='500'
@@ -65,19 +71,27 @@ export default class VideoView extends React.Component {
             </a-text>;
         }
 
-        const videoPlane = <a-plane
-            src={'#' + this.props.src}
-            width={this.props.width}
-            height={this.props.height}
-            scale='0 0 0'
-            position={this.props.position.join(' ')}
-            material='shader: flat; side: double'
-            look-at={this.props.faceCamera ? '[camera]' : null}>
+        const videoPlaneProps = {
+            'width': this.props.width,
+            'height': this.props.height,
+            'scale': '0 0 0',
+            'position': this.props.position.join(' '),
+            'material': 'shader: flat; side: double',
+            'look-at': this.props.faceCamera ? '[camera]' : null,
+        };
+
+        if (this.props.hasVideo) {
+            videoPlaneProps.src = '#' + this.props.src;
+        } else {
+            videoPlaneProps.color = '#444';
+        }
+
+        const videoPlane = <a-plane {...videoPlaneProps} >
             <a-animation
                 attribute='opacity'
                 dur='500'
                 from='0'
-                to={this.props.opacity}></a-animation>
+                to={this.props.hasVideo ? this.props.opacity : 0.75}></a-animation>
             <a-animation
                 attribute='scale'
                 dur='1000'
@@ -97,6 +111,7 @@ export default class VideoView extends React.Component {
 VideoView.defaultProps = {
     faceCamera: true,
     height: 0.9,
+    hasVideo: true,
     opacity: 1.0,
     position: [0, 0, -2],
     rotation: [0, 0, 0],
@@ -107,6 +122,7 @@ VideoView.defaultProps = {
 VideoView.propTypes = {
     faceCamera: React.PropTypes.bool,
     height: React.PropTypes.number,
+    hasVideo: React.PropTypes.bool,
     opacity: React.PropTypes.number,
     position: React.PropTypes.array,
     rotation: React.PropTypes.array,
